237b5f0a7f2f08b4ba7ee46d992eb47a28dc7517
digitaldhiren011-code <digitaldhiren011@gmail.com>
2026-07-30 23:00:30 +0545

    Resize header logo to corner size

--- components/digital-dhiren-logo.tsx
+++ components/digital-dhiren-logo.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function DigitalDhirenLogo() {
   return (
-    <span className="relative block h-14 w-[260px] overflow-hidden sm:h-16 sm:w-[280px] lg:h-[68px] lg:w-[300px]">
+    <span className="relative block h-11 w-[190px] overflow-hidden sm:h-12 sm:w-[205px] lg:h-[56px] lg:w-[220px]">
       <Image
         src="/my-logo.png"
         alt="Digital Dhiren logo"
         fill
         priority
-        sizes="(max-width: 640px) 260px, (max-width: 1024px) 280px, 300px"
+        sizes="(max-width: 640px) 190px, (max-width: 1024px) 205px, 220px"
         className="object-contain object-left"
       />
     </span>
