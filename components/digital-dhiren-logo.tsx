4056e0b20efdf7240ec97b3902be39b94fcf0153
digitaldhiren011-code <digitaldhiren011@gmail.com>
2026-07-30 22:36:45 +0545

    Widen header logo fit

--- components/digital-dhiren-logo.tsx
+++ components/digital-dhiren-logo.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function DigitalDhirenLogo() {
   return (
-    <span className="relative block h-20 w-[300px] overflow-hidden py-1 sm:h-[88px] sm:w-[340px] lg:h-[96px] lg:w-[380px] lg:py-2">
+    <span className="relative block h-16 w-[300px] overflow-hidden sm:h-[76px] sm:w-[340px] lg:h-[84px] lg:w-[380px]">
       <Image
         src="/my-logo.png"
         alt="Digital Dhiren logo"
         fill
         priority
         sizes="(max-width: 640px) 300px, (max-width: 1024px) 340px, 380px"
-        className="object-contain object-center"
+        className="object-contain object-left"
       />
     </span>
   );
