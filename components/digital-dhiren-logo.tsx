bee52deee9791b49f78e90fb48fb3326cdd2b444
digitaldhiren011-code <digitaldhiren011@gmail.com>
2026-07-30 22:56:16 +0545

    Match logo scale to call to action

--- components/digital-dhiren-logo.tsx
+++ components/digital-dhiren-logo.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function DigitalDhirenLogo() {
   return (
-    <span className="relative block h-12 w-[310px] overflow-hidden sm:h-14 sm:w-[345px] lg:h-[60px] lg:w-[380px]">
+    <span className="relative block h-14 w-[260px] overflow-hidden sm:h-16 sm:w-[280px] lg:h-[68px] lg:w-[300px]">
       <Image
         src="/my-logo.png"
         alt="Digital Dhiren logo"
         fill
         priority
-        sizes="(max-width: 640px) 310px, (max-width: 1024px) 345px, 380px"
+        sizes="(max-width: 640px) 260px, (max-width: 1024px) 280px, 300px"
         className="object-contain object-left"
       />
     </span>
