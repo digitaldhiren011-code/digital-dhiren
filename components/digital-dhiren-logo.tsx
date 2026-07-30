da6986f7a0c91cdb6eda07de48be8fbe461ee3b2
digitaldhiren011-code <digitaldhiren011@gmail.com>
2026-07-30 22:31:06 +0545

    Add breathing room to logo

--- components/digital-dhiren-logo.tsx
+++ components/digital-dhiren-logo.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function DigitalDhirenLogo() {
   return (
-    <span className="relative block h-16 w-[280px] overflow-hidden sm:h-[72px] sm:w-[320px] lg:h-20 lg:w-[360px]">
+    <span className="relative block h-20 w-[300px] overflow-hidden py-1 sm:h-[88px] sm:w-[340px] lg:h-[96px] lg:w-[380px] lg:py-2">
       <Image
         src="/my-logo.png"
         alt="Digital Dhiren logo"
         fill
         priority
-        sizes="(max-width: 640px) 280px, (max-width: 1024px) 320px, 360px"
+        sizes="(max-width: 640px) 300px, (max-width: 1024px) 340px, 380px"
         className="object-contain object-center"
       />
     </span>
