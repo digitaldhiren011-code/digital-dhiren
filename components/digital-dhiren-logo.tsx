23dd859e19cde1f12dc2eaf382bd34ff7fd69e38
digitaldhiren011-code <digitaldhiren011@gmail.com>
2026-07-30 22:26:57 +0545

    Increase header logo size

--- components/digital-dhiren-logo.tsx
+++ components/digital-dhiren-logo.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function DigitalDhirenLogo() {
   return (
-    <span className="relative block h-14 w-[240px] overflow-hidden sm:h-16 sm:w-[280px] lg:h-[72px] lg:w-[320px]">
+    <span className="relative block h-16 w-[280px] overflow-hidden sm:h-[72px] sm:w-[320px] lg:h-20 lg:w-[360px]">
       <Image
         src="/my-logo.png"
         alt="Digital Dhiren logo"
         fill
         priority
-        sizes="(max-width: 640px) 220px, (max-width: 1024px) 260px, 300px"
-        className="object-contain object-center p-1"
+        sizes="(max-width: 640px) 280px, (max-width: 1024px) 320px, 360px"
+        className="object-contain object-center"
       />
     </span>
   );
