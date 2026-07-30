e6d7feecd6fa58bc63407961278d3a21fe2b009b
digitaldhiren011-code <digitaldhiren011@gmail.com>
2026-07-30 22:53:22 +0545

    Tighten header spacing

--- components/digital-dhiren-logo.tsx
+++ components/digital-dhiren-logo.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function DigitalDhirenLogo() {
   return (
-    <span className="relative block h-14 w-[320px] overflow-hidden sm:h-[68px] sm:w-[360px] lg:h-[76px] lg:w-[400px]">
+    <span className="relative block h-12 w-[310px] overflow-hidden sm:h-14 sm:w-[345px] lg:h-[60px] lg:w-[380px]">
       <Image
         src="/my-logo.png"
         alt="Digital Dhiren logo"
         fill
         priority
-        sizes="(max-width: 640px) 320px, (max-width: 1024px) 360px, 400px"
+        sizes="(max-width: 640px) 310px, (max-width: 1024px) 345px, 380px"
         className="object-contain object-left"
       />
     </span>
